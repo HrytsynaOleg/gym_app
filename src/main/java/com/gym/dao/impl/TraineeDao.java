53ac3aa40e1d4e726359d70b10ab2ffef02db846
Oleg Hrytsyna <servis535@gmail.com>
2024-09-26 02:51:39 +0300

    upgrade trainee service and dao

--- src/main/java/com/gym/dao/impl/TraineeDao.java
+++ src/main/java/com/gym/dao/impl/TraineeDao.java
@@ -2,8 +2,11 @@ package com.gym.dao.impl;
 
 import com.gym.dao.ITraineeDao;
 import com.gym.entity.Trainee;
+import com.gym.entity.Trainer;
+import com.gym.entity.TrainerTrainee;
 import com.gym.entity.User;
 import com.gym.model.TraineeModel;
+import com.gym.model.TrainerModel;
 import com.gym.utils.Mapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository("traineeDao")
@@ -115,16 +119,92 @@ public class TraineeDao implements ITraineeDao {
         Trainee trainee;
         try {
             trainee = entityManager.find(Trainee.class, id);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Dao error occurred - get trainee");
             return null;
-        }finally {
+        } finally {
             entityManager.close();
         }
         if (trainee == null) {
             return null;
         }
         return Mapper.mapTraineeEntityToTraineeModel(trainee);
+    }
+
+    @Override
+    public void intendTrainer(TraineeModel traineeModel, TrainerModel trainerModel) {
+        Trainer trainer = Mapper.mapTrainerModelToTrainerEntity(trainerModel);
+        Trainee trainee = Mapper.mapTraineeModelToTraineeEntity(traineeModel);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            TrainerTrainee newTrainerTrainee = new TrainerTrainee();
+            newTrainerTrainee.setTrainer(trainer);
+            newTrainerTrainee.setTrainee(trainee);
+            entityManager.getTransaction().begin();
+            entityManager.persist(newTrainerTrainee);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            log.error("Dao error occurred - intend trainer");
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public void deleteTrainer(TraineeModel traineeModel, TrainerModel trainerModel) {
+        Trainer trainer = Mapper.mapTrainerModelToTrainerEntity(trainerModel);
+        Trainee trainee = Mapper.mapTraineeModelToTraineeEntity(traineeModel);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String queryString = "select t from TrainerTrainee t where t.trainee.id like ?1 and " +
+                "t.trainer.id like ?2";
+        TypedQuery<TrainerTrainee> query = entityManager.createQuery(queryString, TrainerTrainee.class);
+        query.setParameter(1, trainee.getId());
+        query.setParameter(2, trainer.getId());
+        List<TrainerTrainee> trainerTraineeList = query.getResultList();
+        try {
+            trainerTraineeList = query.getResultList();
+            if (trainerTraineeList.size() == 1) {
+                entityManager.getTransaction().begin();
+                entityManager.remove(trainerTraineeList.get(0));
+                entityManager.getTransaction().commit();
+            }
+            else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            log.error("Dao error occurred - delete trainer");
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public List<TrainerModel> getIntendedTrainerList(TraineeModel traineeModel) {
+        Trainee trainee = Mapper.mapTraineeModelToTraineeEntity(traineeModel);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        List<TrainerModel> trainerModelList;
+        try {
+            String queryString = "select t from TrainerTrainee t where t.trainee.id like ?1";
+            TypedQuery<TrainerTrainee> query = entityManager.createQuery(queryString, TrainerTrainee.class);
+            query.setParameter(1, trainee.getId());
+            List<TrainerTrainee> trainerTraineeList = query.getResultList();
+            trainerModelList = trainerTraineeList.stream()
+                    .map(TrainerTrainee::getTrainer)
+                    .map(Mapper::mapTrainerEntityToTrainerModel)
+                    .toList();
+        } catch (Exception e) {
+            log.error("Dao error occurred - get trainer list");
+            return new ArrayList<>();
+        } finally {
+            entityManager.close();
+        }
+        return trainerModelList;
     }
 }
 
